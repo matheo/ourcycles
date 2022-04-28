39228b8f1873b467c63cdb4d7cf277d9326468b8
Mateo Tibaquira <nestormateo@gmail.com>
2022-04-28 17:16:28 -0500

    build: nx g @nrwl/angular:app website

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ourcycles-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'website';
+}
