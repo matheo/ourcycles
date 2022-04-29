76b950c8cb0eadf82560a58cf718a54b433b9e7b
Mateo Tibaquira <nestormateo@gmail.com>
2022-04-29 18:26:34 -0500

    build: add @angular/material resources

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { WebLayoutModule, WebLayoutWrapperComponent } from '@ourcycles/web-layout';
 
@@ -20,7 +20,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
     WebLayoutModule,
   ],
